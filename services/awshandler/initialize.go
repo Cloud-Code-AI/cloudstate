8f9d5091681d6c760ce7fac1b6fea15185876bf7
Saurav Panda <sgp65@cornell.edu>
2023-11-29 23:02:49 -0500

    Added region support for cloudstate

--- services/awshandler/initialize.go
+++ services/awshandler/initialize.go
@@ -7,8 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-func StoreAWSData() {
-	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+func StoreAWSData(region string) {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
 		fmt.Println(err)
